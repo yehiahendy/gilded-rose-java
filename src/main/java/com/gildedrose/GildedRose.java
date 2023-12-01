375193485a33cdad5afab98506b9f7d53782429f
yehia <yayahendy11@gmail.com>
2023-12-02 01:49:06 +0200

    Refactore The updateQuality function to Strategy pattern

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import com.gildedrose.update_item_strategy.*;
+
 class GildedRose {
     Item[] items;
 
@@ -8,55 +10,23 @@ class GildedRose {
     }
 
     public void updateQuality() {
+        ItemUpdateStrategy itemUpdateStrategy;
         for (Item item : items) {
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+            itemUpdateStrategy = createItemUpdateStrategy(item);
+            itemUpdateStrategy.update();
+        }
+    }
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            }
+    private ItemUpdateStrategy createItemUpdateStrategy(Item item) {
+        switch (item.name) {
+            case "Sulfuras, Hand of Ragnaros":
+                return new SulfurasUpdateStrategy(item);
+            case "Aged Brie":
+                return new AgedBrieUpdateStrategy(item);
+            case "Backstage passes to a TAFKAL80ETC concert":
+                return new BackStageUpdateStrategy(item);
+            default:
+                return new NormalItemUpdateStrategy(item);
         }
     }
 }
