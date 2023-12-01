c5ca331aaf53bba528d40a07cbb02d35423d75eb
yehia <yayahendy11@gmail.com>
2023-12-02 00:39:38 +0200

    Add NormalItemUpdateStrategy

---
+++ src/main/java/com/gildedrose/update_item_strategy/NormalItemUpdateStrategy.java
@@ -0,0 +1,33 @@
+package com.gildedrose.update_item_strategy;
+
+import com.gildedrose.Item;
+
+public class NormalItemUpdateStrategy implements ItemUpdateStrategy {
+    private final Item item;
+
+    public NormalItemUpdateStrategy(Item item) {
+        this.item = item;
+    }
+
+    @Override
+    public void updateSellIn() {
+        decreaseSellIn();
+    }
+
+    @Override
+    public void updateQuality() {
+        decreaseQuality();
+    }
+
+    @Override
+    public void update() {
+        updateQuality();
+        updateSellIn();
+
+    }
+
+    @Override
+    public Item getItem() {
+        return item;
+    }
+}
